74b072ed94df5414b60174bba18fb5f99dfdd71f
UniDex677 <144460450+UniDex677@users.noreply.github.com>
2026-03-20 20:22:14 +0200

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,5 +1,24 @@
 import os
-from Classes.ServerConnection import ServerConnection
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Server is running')
+
+def start_http_server():
+    port = int(os.environ.get("PORT", 8080))
+    httpd = HTTPServer(('0.0.0.0', port), HealthCheckHandler)
+    httpd.serve_forever()
+
+# Запускаем HTTP-сервер в отдельном потоке
+http_thread = threading.Thread(target=start_http_server, daemon=True)
+http_thread.start()
+
+# Ваш существующий код
+from Classes.ServerConnection import ServerConnection
 port = int(os.environ.get("PORT", 9339))
 ServerConnection(("0.0.0.0", port))
