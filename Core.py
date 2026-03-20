01c23b7761f24a9416cdb8cd0efff8ef98d707ac
UniDex677 <144460450+UniDex677@users.noreply.github.com>
2026-03-20 20:33:46 +0200

    Update Core.py

--- Core.py
+++ Core.py
@@ -21,4 +21,4 @@ http_thread.start()
 # Ваш существующий код
 from Classes.ServerConnection import ServerConnection
 port = int(os.environ.get("PORT", 9339))
-ServerConnection(("0.0.0.0", 10000))
+ServerConnection(("0.0.0.0", 9339))
